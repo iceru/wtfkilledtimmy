6a83b2da55cfd07614244e42a2f137b1437f02fd
Muhamad Hafiz <m.hafiz1825@gmail.com>
2022-04-17 04:29:26 +0700

    add sidebar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,15 +4,17 @@ import Discord from '../../assets/images/discord.png';
 import Twitter from '../../assets/images/twitter.png';
 import Instagram from '../../assets/images/instagram.png';
 import Menu from '../../assets/images/Menu.png';
+import KepoNav from '../../assets/images/kepo-nav.png';
+import CloseNav from '../../assets/images/close-nav.png';
 
 import './style.scss';
-import { Button, Offcanvas, OffcanvasBody, OffcanvasHeader } from "reactstrap";
+import { Offcanvas, OffcanvasBody, OffcanvasHeader } from "reactstrap";
 
 const Navigation = () => {
     const [open, setOpen] = useState(false);
 
     const handleOpen = () => {
-        setOpen(true);
+        setOpen(!open);
     }
 
     const close = () => {
@@ -41,6 +43,27 @@ const Navigation = () => {
                     </div>
                 </div>
             </div>
+            <Offcanvas direction='end' isOpen={open} toggle={() => handleOpen()}>
+                <OffcanvasBody>
+                    <div className="close-nav" onClick={() => handleOpen()}>
+                        <img src={CloseNav} alt='kepo' />
+                    </div>
+                    <div className="kepo">
+                        <img className="w-100" src={KepoNav} alt='kepo' />
+                    </div>
+                    <div className="socials">
+                        <a href="https://discord.gg/mgTURNpJ3v" className="item">
+                            <img src={Discord} alt="Discord" />
+                        </a>
+                        <a href="https://twitter.com/wtfkilledtimmy" className="item">
+                            <img src={Twitter} alt="Twitter" />
+                        </a>
+                        <a href="https://instagram.com/wtfkilledtimmy" className="item">
+                            <img src={Instagram} alt="Instagram" />
+                        </a>
+                    </div>
+                </OffcanvasBody>
+            </Offcanvas>
         </>
     )
 }
